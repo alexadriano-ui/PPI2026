4e9c88680a7bb6aabaa9e7e913ddd96ee9c2ceba
Alex Adriano de Morais Silva <alex.adriano@escolar.ifrn.edu.br>
2026-05-06 19:19:25 +0000

    Aula 06/05 - introdução a JS

---
+++ ppi26.js
@@ -0,0 +1,35 @@
+// Programação para internet
+// Discente: Alex Adriano
+
+// # Aula 06/05 - Introdução ao JavasCript
+console.log("Bem-vindos a ppi!!!");
+
+// Ativar o formatador Prettier
+// ALT + SHIFT + F
+
+// Váriaveis
+let name = "joão";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; // Não pode ser alterada
+console.log(PI);
+
+// Operadores aritméticos
+let x = 5 + 5; // soma
+let y = "5" + 5; // concatenação
+let z = "hello" + 5; //
+
+console.log(x, y, z);
+console.log(typeof x);
+
+// Operadores relacionais
+console.log('5' != 5) // Diferença
+console.log('5' == 5); // Compara tipo ou valor
+console.log('5' === 5); // Compara tipo e valor
+
+// Operadores de incremento
+x = x++;
+console.log(x++); // incrementa após o retorno
+console.log(x);
+console.log(++x); // incrementa antes do retorno
